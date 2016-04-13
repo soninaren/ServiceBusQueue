c6200f8a436c67414717e886304064497fb8b4db
Naren Soni <soninaren@live.com>
2016-04-13 12:37:48 -0700

    Creating ServiceBus Queue Project

---
+++ ServiceBusQueue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connectionString = "CONNECTIONSTRING";
+
+            var namespaceManager =
+                NamespaceManager.CreateFromConnectionString(connectionString);
+            bool queueExists = namespaceManager.QueueExists("QUEUENANE");
+            if (!queueExists)
+            {
+                namespaceManager.CreateQueue("QUEUENANE");
+            }
+
+            QueueClient Client = QueueClient.CreateFromConnectionString(connectionString, "QUEUENANE");
+
+            Client.Send(new BrokeredMessage());
+
+            for (int i = 0; i < 5; i++)
+            {
+                // Create message, passing a string message for the body.
+                BrokeredMessage message = new BrokeredMessage("Test message " + i);
+
+                // Set some addtional custom app-specific properties.
+                message.Properties["TestProperty"] = "TestValue";
+                message.Properties["Message number"] = i;
+
+                // Send message to the queue.
+                Client.Send(message);
+            }
+
+        }
+    }
+}
